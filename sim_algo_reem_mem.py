e0cc41e131d19e475b5880310a4eeaecdddd85bf
Moisés Arrieta <moisesdavidarh@gmail.com>
2025-04-24 20:41:20 -0500

    Implementación de LRU listo3

--- sim_algo_reem_mem.py
+++ sim_algo_reem_mem.py
@@ -67,17 +67,7 @@ def print_results(results):
     for result in results:
         print(f"Req: {result[0]:#0{4}x} Dirección Física: {result[1]:#0{4}x} Acción: {result[2]}")
 
+
 if __name__ == '__main__':
-    
-    marcos_libres = [0x0, 0x1, 0x2]
-    reqs = [0x00, 0x12, 0x64, 0x65, 0x8D, 0x8F, 0x19, 0x18, 0xF1, 0x0B, 0xDF, 0x0A]
-    segmentos = [
-        ('.text', 0x00, 0x1A),
-        ('.data', 0x40, 0x28),
-        ('.heap', 0x80, 0x1F),
-        ('.stack', 0xC0, 0x22),
-    ]
-
-
     results = procesar(segmentos, reqs, marcos_libres)
     print_results(results)
